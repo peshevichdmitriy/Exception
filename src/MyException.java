435a44f9dbcbe951a81acb1c05b529617d3f289c
dmitriypeshevich <peshevichdmitriy@gmail.com>
2025-05-08 23:30:31 +0300

    Final 8.05

--- src/MyException.java
+++ src/MyException.java
@@ -2,8 +2,8 @@ import java.io.IOException;
 
 
 public class MyException extends IOException {
-  public MyException(String message, Throwable ex) {
-    super(message, ex);
+  public MyException(String message, Throwable source) {
+    super(message, source);
   }
 }
 
